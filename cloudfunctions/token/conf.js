e65bf4cb73842502a6c06a1d2e12bddc51cc73a4
Conviction <875603069@qq.com>
2020-02-17 20:21:31 +0800

    2020年2月17日 20:21:29

--- cloudfunctions/token/conf.js
+++ cloudfunctions/token/conf.js
@@ -1,7 +1,7 @@
 const args = {
-  APP_ID:"",
-  API_KEY:"",
-  SECRET_KEY:""
+  APP_ID:"18417855",
+  API_KEY:"quzHlub5pGILrK8CyewIeWXm",
+  SECRET_KEY:"9dvl9BFDKtBzuEtbXhTv8nE7KP3BQpIz"
 }
 
 module.exports = args
